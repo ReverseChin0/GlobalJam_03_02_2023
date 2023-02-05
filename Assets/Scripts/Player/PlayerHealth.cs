dcc8d287be4634200fa0c092fb5782eb9ad0ced4
AzaelSan <azaelsnz@gmail.com>
2023-02-05 13:32:04 -0600

    health icons

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -5,16 +7,19 @@ public class PlayerHealth : MonoBehaviour
     public JicamaAnimManager m_Animations;
     private int m_CurrentHealth;
     [SerializeField] private int m_MaxHealth;
+    [SerializeField] private List<GameObject> m_Icons;
 
     private void Awake()
     {
         m_Animations = GetComponentInChildren<JicamaAnimManager>();
+        m_CurrentHealth = m_MaxHealth;
     }
 
     public void TakeDamage(int a_Amount)
     {
         m_Animations.Hurt();
         m_CurrentHealth -= a_Amount;
+        UpdateView();
         if (m_CurrentHealth <= 0)
             Death();
     }
@@ -22,7 +27,6 @@ public class PlayerHealth : MonoBehaviour
     public void ResetHealth()
     {
         m_Animations.Burrow();
-        Debug.Log("VIDA REGENERADA");
         m_CurrentHealth = m_MaxHealth;
     }
 
@@ -30,5 +34,22 @@ public class PlayerHealth : MonoBehaviour
     {
         //Animacion de muerte
         //Pantalla de muerte
+    }
+
+    private void UpdateView()
+    {
+        for (int i = 0; i < m_Icons.Count; i++)
+        {
+            if (i < m_CurrentHealth)
+            {
+                m_Icons[i].transform.GetChild(0).gameObject.SetActive(true);
+                m_Icons[i].transform.GetChild(1).gameObject.SetActive(false);
+            }
+            else
+            {
+                m_Icons[i].transform.GetChild(0).gameObject.SetActive(false);
+                m_Icons[i].transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
     }
 }
