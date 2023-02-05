56a95624d930845a57f273862a3091058222c867
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2023-02-05 03:36:47 -0600

    jicama

---
+++ Assets/Scripts/CarpetiuxDelJorgiux/JicamaAnimManager.cs
@@ -0,0 +1,301 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine;
+using Spine.Unity;
+
+
+
+public class JicamaAnimManager : MonoBehaviour
+{
+    public SkeletonAnimation Jicama_Skeleton; //Used to control everything realted to the animations.
+    private TrackEntry attackTrackEntry, aimTrackEntry, shootingTrackEntry, dashTrackEntry, hurtTrackEntry;
+    public Collider2D Front_Hand_Collider, Back_Hand_Collider, Front_Foot_Collider, Back_Foot_Collider, Grab_Collider;
+    public int Jump_state = 0;
+    public bool Moving = false;
+    [Range(0.0f, 1.0f)]
+    public float Aim_Fully_Down = 0.5f;
+    public bool Shooting = false;
+    public int Crack_State = 0;
+    public bool Grabbing = false;
+    public int Attack_state = 0;
+
+    private void Start()
+    {
+        Jicama_Skeleton.AnimationState.Event += HandleEvent; //To listen to animation events via void HandleEvent(TrackEntry trackEntry, Spine.Event e)
+    }
+
+    private void Update()
+    {
+        if (Shooting)
+        {
+            aimTrackEntry.Alpha = Aim_Fully_Down;
+        }
+    }
+
+    void HandleEvent(TrackEntry trackEntry, Spine.Event e)
+    {
+        if (e.Data.Name == "Atk_Front_Hand")
+        {
+            if(Front_Hand_Collider.enabled)
+            {
+                Front_Hand_Collider.enabled = false;
+            }
+            else
+            {
+                Front_Hand_Collider.enabled = true;
+            }
+        }
+
+        if (e.Data.Name == "Atk_Back_Hand")
+        {
+            if (Back_Hand_Collider.enabled)
+            {
+                Back_Hand_Collider.enabled = false;
+            }
+            else
+            {
+                Back_Hand_Collider.enabled = true;
+            }
+        }
+
+        if (e.Data.Name == "Atk_Front_Foot")
+        {
+            if (Front_Hand_Collider.enabled)
+            {
+                Front_Hand_Collider.enabled = false;
+            }
+            else
+            {
+                Front_Hand_Collider.enabled = true;
+            }
+        }
+
+        if (e.Data.Name == "Atk_Back_Foot")
+        {
+            if (Back_Foot_Collider.enabled)
+            {
+                Back_Foot_Collider.enabled = false;
+            }
+            else
+            {
+                Back_Foot_Collider.enabled = true;
+            }
+        }
+
+        if (e.Data.Name == "Shoot")
+        {
+
+        }
+
+        if (e.Data.Name == "Grab")
+        {
+            if (Grab_Collider.enabled)
+            {
+                Grab_Collider.enabled = false;
+            }
+            else
+            {
+                Grab_Collider.enabled = true;
+            }
+        }
+
+        if (e.Data.Name == "Burrowed")
+        {
+            Crack_State = 0;
+            Jicama_Skeleton.AnimationState.SetEmptyAnimation(3, 0.25f);
+        }
+    }
+
+    public void LightAttack()
+    {
+        //tiltTrackEntry.MixBlend = MixBlend.Add;
+        switch(Attack_state)
+        {
+            case 1:
+                attackTrackEntry = Jicama_Skeleton.AnimationState.AddAnimation(4, "Attack_Light_B", false, 0);
+                Attack_state = 2;
+                break;
+            case 2:
+                attackTrackEntry = Jicama_Skeleton.AnimationState.AddAnimation(4, "Attack_Light_C", false, 0);
+                Attack_state = 0;
+                break;
+            default:
+                attackTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(4, "Attack_Light_A", false);
+                Attack_state = 1;
+                break;
+        }
+        attackTrackEntry.Complete += delegate //To run what's inside the brackets when the last queued animation finishes.
+        {
+            Jicama_Skeleton.AnimationState.SetEmptyAnimation(4, 0.25f);
+            attackTrackEntry = null;
+        };
+
+    }
+    public void HeavyAttack()
+    {
+        if(Jump_state != 0) //Aerial
+        {
+            attackTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(4, "Attack_Heavy_Air_A", false);
+        }
+        else
+        {
+            attackTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(4, "Attack_Heavy_Ground_A", false);
+        }
+        attackTrackEntry.Complete += delegate //To run what's inside the brackets when the last queued animation finishes.
+        {
+            Jicama_Skeleton.AnimationState.SetEmptyAnimation(4, 0.25f);
+            attackTrackEntry = null;
+        };
+    }
+    public void Jump()
+    {
+        if(Jump_state == 0)
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(0, "Jump", false);
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Airborne_Up", true, 0.0f);
+            Jump_state = 1;
+        }
+        else if (Jump_state == 1)
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(0, "Jump_Extra", false);
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Airborne_Up", true, 0.0f);
+        }
+    }
+    public void Crack()
+    {
+        switch(Crack_State)
+        {
+            case 0:
+                Jicama_Skeleton.AnimationState.SetAnimation(3, "Pot_Crack_0", false);
+                Crack_State = 1;
+                break;
+            case 1:
+                Jicama_Skeleton.AnimationState.SetAnimation(3, "Pot_Crack_1", false);
+                Crack_State = 2;
+                break;
+            case 2:
+                Jicama_Skeleton.AnimationState.SetAnimation(0, "Death", false);
+                Crack_State = 3;
+                break;
+            default:
+                if (Moving)
+                {
+                    Jicama_Skeleton.AnimationState.AddAnimation(0, "Move", true, 0.0f);
+                }
+                else
+                {
+                    Jicama_Skeleton.AnimationState.AddAnimation(0, "Idle", true, 0.0f);
+                }
+                break;
+        }
+    }
+    public void Burrow()
+    {
+        Jicama_Skeleton.AnimationState.SetAnimation(0, "Burrow_and_New_Pot", false);
+        if (Moving)
+        {
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Move", true, 0.0f);
+        }
+        else
+        {
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Idle", true, 0.0f);
+        }
+    }
+    public void Hurt()
+    {
+        hurtTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(2, "Hurt", false); //"Dash_Backward" also exists.
+        hurtTrackEntry.Complete += delegate //To run what's inside the brackets when the last queued animation finishes.
+        {
+             Jicama_Skeleton.AnimationState.SetEmptyAnimation(2, 0.25f);
+            hurtTrackEntry = null;
+        };
+    }
+    public void Grab()
+    {
+        if(!Grabbing)
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(4, "Grab", false);
+            Jicama_Skeleton.AnimationState.AddAnimation(4, "Grab_Idle", true, 0.0f);
+            Grabbing = true;
+        }
+        else
+        {
+            attackTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(4, "Toss", false);
+            attackTrackEntry.Complete += delegate //To run what's inside the brackets when the last queued animation finishes.
+            {
+                Jicama_Skeleton.AnimationState.SetEmptyAnimation(4, 0.25f);
+                attackTrackEntry = null;
+            };
+            Grabbing = false;
+        }
+    }
+    public void Move()
+    {
+        if(Moving)
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(0, "Idle", true);
+            Moving = false;
+        }
+        else
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(0, "Move", true);
+            Moving = true;
+        }
+    }
+    public void Turret()
+    {
+        if(!Shooting)
+        {
+            Shooting = true;
+            Jicama_Skeleton.AnimationState.SetAnimation(5, "Turret_On", false);
+            Jicama_Skeleton.AnimationState.AddAnimation(5, "Turret_Shoot", true, 0.0f);
+            aimTrackEntry = Jicama_Skeleton.AnimationState.AddAnimation(6, "Turret_Aim_Full_Down", true, 0.0f);
+            aimTrackEntry.MixBlend = MixBlend.Replace;
+            aimTrackEntry.Alpha = Aim_Fully_Down;
+        }
+        else
+        {
+            Shooting = false;
+            shootingTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(5, "Turret_Off", false);
+            shootingTrackEntry.Complete += delegate //To run what's inside the brackets when the last queued animation finishes.
+            {
+                Jicama_Skeleton.AnimationState.SetEmptyAnimation(5, 0.25f);
+                Jicama_Skeleton.AnimationState.SetEmptyAnimation(6, 0.25f);
+                shootingTrackEntry = null;
+                aimTrackEntry = null;
+            };
+        }
+    }
+    public void Dash()
+    {
+        dashTrackEntry = Jicama_Skeleton.AnimationState.SetAnimation(1, "Dash_Forward", false); //"Dash_Backward" also exists.
+        dashTrackEntry.Complete += delegate //To run what's inside the brackets when the last queued animation finishes.
+        {
+            Jicama_Skeleton.AnimationState.SetEmptyAnimation(1, 0.25f);
+            dashTrackEntry = null;
+        };
+        if (Moving)
+        {
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Move", true, 0.0f);
+        }
+        else
+        {
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Idle", true, 0.0f);
+        }
+    }
+    public void Fall()
+    {
+        if (Jump_state == 2)
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(0, "Land", false);
+            Jicama_Skeleton.AnimationState.AddAnimation(0, "Idle", true, 0.0f);
+        }
+        else if (Jump_state != 0)
+        {
+            Jicama_Skeleton.AnimationState.SetAnimation(0, "Airborne_Down", true);
+            Jump_state = 2;
+        }
+    }    
+
+}
--- Assets/Scripts/GrabComponent.cs
+++ Assets/Scripts/GrabComponent.cs
@@ -4,98 +4,6 @@ using UnityEngine.InputSystem;
 
 public class GrabComponent : MonoBehaviour
 {
-    //public GameObject Mano;
-    //GameObject enemigoAgarrado;
-    //Animator AnimatorHand;
-    //public Collider2D triggermano;
-    //public Transform reticula;
-    //SpringJoint2D miSpring;
-
-    //Vector2 currdirection, currentpos, posiAim, lastpos;
-    //float cangrabTime = 0.0f;
-    //bool cangrab = true;
-    //[HideInInspector]
-    //public bool Isgrabbing = false;
-    //private void Start()
-    //{
-    //    triggermano.enabled = false;
-    //}
-    //void GrabHandler()
-    //{
-    //    if (Input.GetAxis("GrabTrigg") == 1 || Input.GetAxis("Fire3") == 1)
-    //    {
-    //        triggermano.enabled = true;
-    //        Mano.transform.position = new Vector3(reticula.position.x, reticula.position.y, Mano.transform.position.z);
-    //        Mano.transform.rotation = Quaternion.LookRotation(posiAim, Vector3.forward);
-    //        Mano.transform.Rotate(90, 0, 0);
-
-
-    //        if (cangrab)
-    //        {
-    //            cangrabTime = Time.time;
-    //            cangrab = false;
-    //            AnimatorHand.SetBool("brazoBool", true);
-
-    //            if (Isgrabbing)
-    //            {
-    //                Mano.transform.position = new Vector3(reticula.position.x, reticula.position.y, Mano.transform.position.z);
-    //                Mano.transform.rotation = Quaternion.LookRotation(posiAim, Vector3.forward);
-    //                Mano.transform.Rotate(90, 0, 0);
-    //                cangrab = false;
-    //                ThrowSomething();
-    //            }
-    //        }
-
-    //    }
-
-
-    //    if (!cangrab)
-    //    {
-    //        if (Time.time - cangrabTime > 0.2f)
-    //        {
-    //            triggermano.enabled = false;
-    //            AnimatorHand.SetBool("brazoBool", false);
-    //            cangrab = true;
-    //        }
-    //    }
-    //}
-
-    //public void grabbedSomething(Collider2D elagarrado)
-    //{
-    //    enemigoAgarrado = elagarrado.gameObject;
-    //    miSpring.connectedBody = elagarrado.attachedRigidbody;
-    //    Enemie enemigo = elagarrado.gameObject.GetComponent<Enemie>();
-    //    enemigo.mienemimanager.disminuirenemigos();
-    //    enemigo.enabled = false;
-    //    //elagarrado.gameObject.GetComponent<CircleCollider2D>().enabled = false;
-    //    elagarrado.isTrigger = true;
-    //    enemigoAgarrado.tag = "Escudo";
-    //    miSpring.enabled = true;
-    //    Isgrabbing = true;
-    //}
-
-    //public void ThrowSomething()
-    //{
-    //    enemigoAgarrado.tag = "Lanzado";
-    //    miSpring.connectedBody = null;//spring joint suelta al objeto
-    //    CircleCollider2D micircle = enemigoAgarrado.GetComponent<CircleCollider2D>();//obtenemos el collider
-    //    micircle.radius = 0.5f;
-    //    //micircle.isTrigger = true;  //lo convierte en trigger
-    //    micircle.enabled = true;    //lo activa
-    //    miSpring.enabled = false;   //apaga el spring joint
-    //    Rigidbody2D enemigeishon = enemigoAgarrado.GetComponent<Rigidbody2D>();//obtenemos rigidbody del obj
-    //    enemigeishon.velocity = new Vector2(0, 0); //igualamos su velocidad a 0
-    //    enemigeishon.AddForce(posiAim * 1000f); //lo disparamos
-    //    Isgrabbing = false; //ya no esta agarrando
-    //    StartCoroutine(DestruirEnemigo(enemigoAgarrado));
-    //}
-
-    //public IEnumerator DestruirEnemigo(GameObject enemigo)
-    //{
-    //    yield return new WaitForSeconds(2.0f);
-    //    enemigo.SetActive(false);
-    //}
-
     bool _isGrabbing = false;
     float _grabCoolDown = 1, _timeSinceGrabTry=0;
 
@@ -107,6 +15,7 @@ public class GrabComponent : MonoBehaviour
     float _grabDistance = 5.0f, _grabTryDuration=0.8f;
 
     private PlayerMovement _Movement;
+    GameObject _enemieGrabbed = default;
 
     private void Awake()
     {
@@ -142,7 +51,8 @@ public class GrabComponent : MonoBehaviour
 
     void ThrowWhatIsGrabbed()
     {
-
+        Vector3 grabOrientation = _Movement.IsFacingRight ? Vector2.right : Vector2.left;
+        //_enemieGrabbed.transform
     }
 
     IEnumerator TryGrabWhatCanBeThrowned()
@@ -157,10 +67,21 @@ public class GrabComponent : MonoBehaviour
             _rootHook.position = Vector3.Lerp(transform.position,
                                     transform.position + grabOrientation * _grabDistance,
                                     t);
-            
+            if (!_isGrabbing)
+            {
+                Collider2D col = Physics2D.OverlapCircle(_rootHook.position, 2, _layerEnemies);
+                if (col != null)
+                {
+                    col.transform.parent = _rootHook.transform;
+                    _isGrabbing = true;
+                    _enemieGrabbed = col.gameObject;
+                    //col.transform.localScale *= 0.85f;
+                    col.enabled = false;
+                }
+            }                
             yield return null;
         }
-        elapsedTime = t = 0;
+         elapsedTime = t = 0;
         while (t <= 1)
         {
             elapsedTime += Time.deltaTime;
@@ -168,7 +89,6 @@ public class GrabComponent : MonoBehaviour
             _rootHook.position = Vector3.Lerp(transform.position,
                                     transform.position + grabOrientation * _grabDistance,
                                     1-t);
-
             yield return null;
         }
 
